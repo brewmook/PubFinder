519b1bc6ef7c3523e1711e5fbf531c3745fb839c
Dave E <dave.git.2012@coolhandmook.com>
2013-06-16 22:48:26 +0100

    Replace _markers array with individual points for centre and circumference

--- src/lib/L.CircleEditor.js
+++ src/lib/L.CircleEditor.js
@@ -23,7 +23,6 @@ L.CircleEditor = L.Circle.extend ({
         L.Path.prototype.onRemove.call(this, map);
     },
 
-
     addHooks: function () {
         if (this._map) {
             if (!this._markerGroup) {
@@ -37,7 +36,8 @@ L.CircleEditor = L.Circle.extend ({
         if (this._map) {
             this._map.removeLayer(this._markerGroup);
             delete this._markerGroup;
-            delete this._markers;
+            delete this._centerMarker;
+            delete this._circumferenceMarker;
         }
     },
 
@@ -50,19 +50,16 @@ L.CircleEditor = L.Circle.extend ({
         var circleBounds = this.getBounds();
         var swCoord = circleBounds.getSouthWest();
         var neCoord = circleBounds.getNorthEast();
-        return new L.LatLng(neCoord.lat, (neCoord.lng + swCoord.lng) / 2, true);
+        return new L.LatLng((neCoord.lat + swCoord.lat) / 2, neCoord.lng, true);
     },
 
     _initMarkers: function () {
         this._markerGroup = new L.LayerGroup();
-        this._markers = [];
 
-        var markerCenter = this._createMarker(this._latlng, 0, true);
-        markerCenter.on('click', this._onCenterMarkerClick, this);
-        this._markers.push(markerCenter);
+        this.centerMarker = this._createMarker(this._latlng, 0, true);
+        this.centerMarker.on('click', this._onCenterMarkerClick, this);
 
-        var markerNorthCenter = this._createMarker(this._boundaryCoord(), 1);
-        this._markers.push(markerNorthCenter);
+        this._circumferenceMarker = this._createMarker(this._boundaryCoord(), 1);
     },
 
     _createMarker: function (latlng, index, isCenter) {
@@ -74,7 +71,6 @@ L.CircleEditor = L.Circle.extend ({
         if (isCenter === undefined) {
             isCenter = false;
         }
-        //console.log("this is center point: " + isCenter);
 
         marker._origLatLng = latlng;
         marker._index = index;
@@ -99,40 +95,32 @@ L.CircleEditor = L.Circle.extend ({
 
     _onCenterMove: function (e) {
         var marker = e.target;
-        //console.log("center move - START");
 
         L.Util.extend(marker._origLatLng, marker._latlng);
 
-        var mm = this._markers[1];
-        mm.setOpacity(0.1);
+        this._circumferenceMarker.setOpacity(0.1);
 
         this.redraw();
-
-        //console.log("END");
     },
 
     _onCenterMoveEnd: function (e) {
         var marker = e.target;
 
-        var mm = this._markers[1];
-        mm.setLatLng(this._boundaryCoord());
-        mm.setOpacity(1);
+        this._circumferenceMarker.setLatLng(this._boundaryCoord());
+        this._circumferenceMarker.setOpacity(1);
 
         this.fire('centerchange');
     },
 
     _onMarkerDrag: function (e) {
         var marker = e.target;
-        //console.log("marker drag - START");
-        var center = this._markers[0].getLatLng();
         var axis = marker._latlng;
 
-        var distance = center.distanceTo(axis);
+        var distance = this.centerMarker.getLatLng().distanceTo(axis);
 
         this.setRadius(distance);
 
         this.redraw();
-        //console.log("END");
 
         this.fire('radiuschange');
     },
